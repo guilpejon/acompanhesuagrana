3b14594fe7b001d9304c83cf22bae20ab99d4aa9
Guilherme Ferratti Pejon <guilpejon@gmail.com>
2026-03-03 19:50:18 -0300

    Increased timeout

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -191,7 +191,7 @@ Devise.setup do |config|
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
   # time the user will be asked for credentials again. Default is 30 minutes.
-  config.timeout_in = 24.hours
+  config.timeout_in = 1.month
 
   # ==> Configuration for :lockable
   # TODO: Enable once a mailer is configured. To activate:
