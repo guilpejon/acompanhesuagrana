8ce7d08b752b07ed41bb9f85590faa1def935579
Guilherme Pejon <guilherme.pejon@housecallpro.com>
2026-03-12 19:02:37 -0300

    Improved expenses list UI

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   belongs_to :user
   has_many :expenses, dependent: :nullify
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 20 }
   validates :color, presence: true
   validates :icon, presence: true
 end
